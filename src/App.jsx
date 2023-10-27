be589e10ba49113da0d46e3ed1fe831474904495
Saurav Nautiyal <psynyt.learning@gmail.com>
2023-10-27 13:47:31 +0530

    App.jsx Header correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
-import Header from "./components/header";
+import Header from "./components/Header";
 import About from './components/About';
 
 function App() {
